ba4f8d4ce15de66cc2290e02a65b517da4f3209f
Renato Santos <renatoacsantos@gmail.com>
2024-05-20 15:35:49 -0400

    Unfinished changes in correlation model/methods

--- CoNekT_Grasses_Microbiome/conekt/models/expression_microbiome/expression_microbiome_correlation.py
+++ CoNekT_Grasses_Microbiome/conekt/models/expression_microbiome/expression_microbiome_correlation.py
@@ -3,11 +3,25 @@ from conekt import db
 SQL_COLLATION = 'NOCASE' if db.engine.name == 'sqlite' else ''
 
 
+class ExpMicroCorrelationMethod(db.Model):
+    __tablename__ = 'expression_microbiome_correlation_methods'
+    id = db.Column(db.Integer, primary_key=True)
+    stat_method = db.Column(db.Enum('pearson', 'spearman', name='Statistical Method'), default='pearson')
+    multiple_test_cor_method = db.Column(db.Enum('fdr_bh', 'bonferroni', name='P-value correction Method'), default='fdr_bh')
+
+    def __init__(self, stat_method, study_id):
+        self.stat_method = stat_method
+        self.study_id = study_id
+    
+    def __repr__(self):
+        return str(self.id)
+
+
 class ExpMicroCorrelation(db.Model):
     __tablename__ = 'expression_microbiome_correlations'
     id = db.Column(db.Integer, primary_key=True)
-    stat_method = db.Column(db.Enum('pearson', 'spearman', name='Statistical Method'), default='pearson')
     pvalue = db.deferred(db.Column(db.Float))
+    qvalue = db.deferred(db.Column(db.Float))
     corr_coef = db.deferred(db.Column(db.Float))
 
     expression_profile_id = db.Column(db.Integer, db.ForeignKey('expression_profiles.id', ondelete='CASCADE'), index=True)
