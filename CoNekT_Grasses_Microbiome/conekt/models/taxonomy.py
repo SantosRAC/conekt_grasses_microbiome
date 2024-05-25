83d161cd062a3dcfd29d7568ef4f3278c6bfec63
Renato Santos <renatoacsantos@gmail.com>
2024-05-25 19:07:27 -0400

    Getting NCBI taxid for GG; updating foreign key

--- CoNekT_Grasses_Microbiome/conekt/models/taxonomy.py
+++ CoNekT_Grasses_Microbiome/conekt/models/taxonomy.py
@@ -6,7 +6,7 @@ SQL_COLLATION = 'NOCASE' if db.engine.name == 'sqlite' else ''
 class SILVATaxon(db.Model):
     __tablename__ = 'silva_taxonomy'
     id = db.Column(db.Integer, primary_key=True)
-    ncbi_taxid = db.Column(db.Integer, default=0)
+    ncbi_taxid = db.Column(db.Integer, db.ForeignKey('ncbi_taxonomy.id', ondelete='SET NULL'), index=True)
     taxon_path = db.Column(db.String(255), default='')
     rank = db.Column(db.String(255), default='')
 
@@ -50,9 +50,17 @@ class SILVATaxon(db.Model):
                 # Columns 4 and 5 are not used (remark and release)
                 line = line.strip().split('\t')
 
+                # check if the taxon is already in the database
+                ncbi_record = NCBITaxon.query.filter_by(taxid=int(line[1])).first()
+
+                ncbi_record_id = None
+
+                if ncbi_record:
+                    ncbi_record_id = ncbi_record.id
+
                 # add the taxonomy information to the database
                 new_taxon = SILVATaxon(**{"taxon_path": line[0],
-                                          "ncbi_taxid": line[1],
+                                          "ncbi_taxid": ncbi_record_id,
                                           "rank": line[2]})
 
                 db.session.add(new_taxon)
@@ -198,6 +206,23 @@ class NCBITaxon(db.Model):
         return taxon_count
 
     @staticmethod
+    def get_ncbi_taxid_from_taxon(taxon):
+
+        """
+        Get the NCBI taxid from a taxon name
+
+        :param taxon: Taxon name
+        :return: NCBI taxid
+        """
+
+        ncbi_record = NCBITaxon.query.filter_by(taxonomic_name=taxon).first()
+
+        if ncbi_record:
+            return ncbi_record
+        else:
+            return None
+
+    @staticmethod
     def update_counts():
         """
         TODO: implement update counts for NCBI taxonomy
@@ -207,10 +232,12 @@ class GGTaxon(db.Model):
     __tablename__ = 'gg_taxonomy'
     id = db.Column(db.Integer, primary_key=True)
     gg_id = db.Column(db.Integer, default=0)
+    ncbi_taxid = db.Column(db.Integer, db.ForeignKey('ncbi_taxonomy.id', ondelete='SET NULL'), index=True)
     taxon_path = db.Column(db.String(255), default='')
 
-    def __init__(self, gg_id, taxon_path):
+    def __init__(self, gg_id, ncbi_taxid, taxon_path):
         self.gg_id = gg_id
+        self.ncbi_taxid = ncbi_taxid
         self.taxon_path = taxon_path
     
     def __repr__(self):
@@ -247,8 +274,19 @@ class GGTaxon(db.Model):
                 # split the line into the taxonomy information
                 line = line.strip().split('\t')
 
+                lrt = GGTaxon.get_lowest_rank_taxon(line[1])
+
+                ncbi_record = NCBITaxon.get_ncbi_taxid_from_taxon(lrt)
+
+                ncbi_record_id = None
+
+                if ncbi_record:
+                    ncbi_record_id = ncbi_record.id
+
                 # add the taxonomy information to the database
-                new_taxon = GGTaxon(**{"gg_id": line[0], "taxon_path": line[1]})
+                new_taxon = GGTaxon(**{"gg_id": line[0],
+                                       "ncbi_taxid": ncbi_record_id,
+                                       "taxon_path": line[1]})
 
                 db.session.add(new_taxon)
                 new_taxons.append(new_taxon)
@@ -263,6 +301,48 @@ class GGTaxon(db.Model):
         db.session.commit()
 
         return taxon_count
+
+    @staticmethod
+    def get_lowest_rank_taxon(taxon_path):
+
+        """
+        Get the lowest rank taxon with information from GreenGenes taxonomy path
+
+        :param taxon_path: Taxon path from GreenGenes taxonomy
+        """
+
+        lowest_rank_taxon = {'name': None, 'rank': None}
+
+        phylum_name = taxon_path.split(';')[-6].split('__')[-1]
+        class_name = taxon_path.split(';')[-5].split('__')[-1]
+        order_name = taxon_path.split(';')[-4].split('__')[-1]
+        family_name = taxon_path.split(';')[-3].split('__')[-1]
+        genus_name = taxon_path.split(';')[-2].split('__')[-1]
+        species_name = taxon_path.split(';')[-1].split('__')[-1]
+
+        if species_name:
+            lowest_rank_taxon['name'] = genus_name+' '+species_name
+            lowest_rank_taxon['rank'] = 'species'
+        elif genus_name:
+            lowest_rank_taxon['name'] = genus_name
+            lowest_rank_taxon['rank'] = 'genus'
+        elif family_name:
+            lowest_rank_taxon['name'] = family_name
+            lowest_rank_taxon['rank'] = 'family'
+        elif order_name:
+            lowest_rank_taxon['name'] = order_name
+            lowest_rank_taxon['rank'] = 'order'
+        elif class_name:
+            lowest_rank_taxon['name'] = class_name
+            lowest_rank_taxon['rank'] = 'class'
+        elif phylum_name:
+            lowest_rank_taxon['name'] = phylum_name
+            lowest_rank_taxon['rank'] = 'phylum'
+        else:
+            lowest_rank_taxon['name'] = taxon_path.split(';')[-7]
+            lowest_rank_taxon['rank'] = 'kingdom'
+
+        return lowest_rank_taxon['name']
 
     @staticmethod
     def update_counts():
