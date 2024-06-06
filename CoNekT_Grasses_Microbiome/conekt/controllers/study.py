e1f48f4f6310e1adf5de47ad840acfb72450ce6f
Renato Santos <renatoacsantos@gmail.com>
2024-06-06 11:26:16 -0400

    Drafting study view and studies overview

--- CoNekT_Grasses_Microbiome/conekt/controllers/study.py
+++ CoNekT_Grasses_Microbiome/conekt/controllers/study.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, g, jsonify
+from markdown import markdown
 
 from conekt import db, cache
 
@@ -9,6 +10,42 @@ from conekt.models.seq_run import SeqRun
 from conekt.models.studies import Study
 
 study = Blueprint('study', __name__)
+
+
+@study.route('/')
+@cache.cached()
+def studies_overview():
+    """
+    Overview of all studies with data in the current database, including some basic statistics
+    """
+    all_studies = Study.query.all()
+
+    '''for species in all_species:
+        if LiteratureItem.query.filter_by(id=species.literature_id).first():
+            species.paper_author_names = LiteratureItem.query.filter_by(id=species.literature_id).first().author_names
+            species.paper_public_year = LiteratureItem.query.filter_by(id=species.literature_id).first().public_year
+            species.paper_doi = LiteratureItem.query.filter_by(id=species.literature_id).first().doi
+    '''    
+
+    return render_template('study.html', all_studies=all_studies)
+
+
+@study.route('/view/<study_id>')
+@cache.cached()
+def study_view(study_id):
+    """
+    Get a study based on the ID and show the details for this study.
+
+    :param study_id: ID of the study to show
+    """
+    current_study = db.session.get(Study, study_id)
+
+    description = None if current_study.description is None \
+        else markdown(current_study.description, extensions=['markdown.extensions.tables', 'markdown.extensions.attr_list'])
+
+    return render_template('study.html', study=current_study, description=description)
+    
+
 
 @study.route('/studies_papers/<study_id>/')
 @study.route('/studies_papers/<study_id>/<int:page>')
