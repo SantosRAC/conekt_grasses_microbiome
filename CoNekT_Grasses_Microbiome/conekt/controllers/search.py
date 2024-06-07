bf860d9a20e3c225da3b7cacc2dd6d50a82bbbad
Renato Santos <renatoacsantos@gmail.com>
2024-06-07 14:02:42 -0400

    First version of showing correlation results

--- CoNekT_Grasses_Microbiome/conekt/controllers/search.py
+++ CoNekT_Grasses_Microbiome/conekt/controllers/search.py
@@ -5,6 +5,7 @@ search = Blueprint('search', __name__)
 
 from conekt.forms.omics_integration.profile_correlations import SearchCorrelatedProfilesForm
 from conekt.models.species import Species
+from conekt.models.studies import Study
 from conekt.models.expression_microbiome.expression_microbiome_correlation import\
     ExpMicroCorrelationMethod, ExpMicroCorrelation
 
@@ -24,16 +25,17 @@ def search_correlated_profiles():
     else:
         species_id = request.form.get('species_id')
         study_id = request.form.get('study_id')
-        method_id = request.form.get('method')
+        method_id = request.form.get('method_id')
         cutoff = request.form.get('cutoff')
 
         species = Species.query.get_or_404(species_id)
-        exp_micro_cor_method = ExpMicroCorrelationMethod.query.get_or_404(method_id)
-        results = ExpMicroCorrelation.query.\
-            filter(ExpMicroCorrelation.method_id == method_id).\
-            filter(ExpMicroCorrelation.score>=cutoff).\
-            options(
-                joinedload(ExpMicroCorrelation.profile).undefer("profile")
-            )
+        study = Study.query.get_or_404(study_id)
 
-        return render_template("omics_integration/find_expression_microbiome_correlations.html", results=results, species=species_id, method=method_id)
+        results = ExpMicroCorrelation.query.\
+            filter(ExpMicroCorrelation.exp_micro_correlation_method_id == method_id).\
+            filter(ExpMicroCorrelation.corr_coef>=cutoff)
+
+        return render_template("omics_integration/find_expression_microbiome_correlations.html",
+                               results=results,
+                               species=species,
+                               study=study)
