65d1a573368e1ed01ba0a2df9121a9b933f1ac8f
Renato Santos <renatoacsantos@gmail.com>
2024-09-01 10:55:35 -0400

    Adapting the script to parse other ontologies

--- CoNekT_Grasses_Microbiome/scripts/miscellaneous/peco_to_table.py
+++ CoNekT_Grasses_Microbiome/scripts/miscellaneous/peco_to_table.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
 
-with open('peco.obo') as f:
+import argparse
 
+parser = argparse.ArgumentParser(description='Convert the OBO ontology file to a table')
+parser.add_argument('-i', '--input_obo', type=str, dest='obo_file', metavar='envo.obo',
+                    help='The OBO file to convert', required=True)
+parser.add_argument('-o', '--output', type=str, dest='output_file', metavar='output.tsv',
+                    help='The output file to write the table to', required=True)
+parser.add_argument('--ontology', type=str, dest='ontology_type', metavar='ENVO',
+                    choices=['PECO', 'ENVO', 'PO'], help='Ontology (e.g. \'PECO\', \'ENVO\' or \'PO\')', required=True)
+args = parser.parse_args()
+
+obo_file = args.obo_file
+output_table = args.output_file
+ontology = args.ontology_type
+
+output_table_obj = open(output_table, 'w')
+
+output_table_obj.write("id\tname\tdefn\n")
+
+with open(obo_file) as f:
+
+    typedef = 0
     current_term = 0
     printed_to_file = 1
-    peco_id = ''
-    peco_name = ''
+    ontology_id = ''
+    ontology_name = ''
+    ontology_def = ''
 
     for line in f:
         # Skip empty
         line = line.rstrip('\n')
 
         if not line:
-            if (not printed_to_file) and peco_id and peco_name:
-                print(f'{peco_id}\t{peco_name}\tno_peco_definition')
+            if (not printed_to_file) and ontology_id and ontology_name:
+                output_table_obj.write(ontology_id+"\t"+ontology_name+"\tno_ontology_definition\n")
             current_term = 0
-            peco_id = ''
-            peco_name = ''
-            peco_def = ''
+            ontology_id = ''
+            ontology_name = ''
+            ontology_def = ''
             printed_to_file = 0
+            typedef = 0
             continue
 
         if line == "[Term]":
@@ -27,27 +49,60 @@ with open('peco.obo') as f:
                 print(f'Something wrong. Current term ({current_term}) exists.')
                 exit(1)
             current_term = 1
+        
+        if line == "[Typedef]":
 
-        if line.startswith('id: PECO:') and current_term:
-                peco_id = line.replace('id: ', '')
+            if current_term or typedef:
+                print(f'Something wrong. Current typedef ({typedef}) or a current term ({current_term}) exists.')
+                exit(1)
+            typedef = 1
 
-        if line.startswith("name: ") and current_term:
-
-            if peco_id:
-                peco_name = line.replace('name: ', '')
+        if line.startswith(f'id: {ontology}:'):
+            if current_term:
+                ontology_id = line.replace('id: ', '')
             else:
-                print(f'Something wrong with PECO ID {peco_name}')
+                #ontology_id = line.replace('id: ', '')
+                #print(f'Something wrong. Found ID ({ontology_id}), but [Term] flag was not recorded.')
+                # Here are cases with typedefs as I could note. We are not interested in them.
+                continue
 
-        if line.startswith("def: ") and current_term:
-
-            if peco_id:
-                #and peco_name:
-                peco_def = line.replace('def: ', '')
-                print(f'{peco_id}\t{peco_name}\t{peco_def}')
-                printed_to_file = 1
-                peco_id = ''
-                peco_name = ''
-                peco_def = ''
-                current_term = 0
+        if line.startswith("name: "):
+            
+            if current_term:
+                if ontology_id:
+                    ontology_name = line.replace('name: ', '')
+                else:
+                    continue
+                    #ontology_name = line.replace('name: ', '')
+                    #print(f'Something wrong. Found Name ({ontology_name}), but no ID was recorded.')
             else:
-                print(f'Something is wrong with PECO ID {peco_id}')
+                #ontology_name = line.replace('name: ', '')
+                #print(f'Something wrong. Found Name ({ontology_name}), but [Term] flag was not recorded. ({ontology_id})')
+                continue
+
+        if line.startswith("def: "):
+        
+            if current_term:
+
+                if typedef: # Skip typedefs (relations). We are interested in the nodes in the current implementation
+                    continue
+
+                if ontology_id and ontology_name:
+                    ontology_def = line.replace('def: ', '')
+                    output_table_obj.write(ontology_id+"\t"+ontology_name+"\t"+ontology_def+"\n")
+                    printed_to_file = 1
+                    ontology_id = ''
+                    ontology_name = ''
+                    ontology_def = ''
+                    current_term = 0
+                else:
+                    continue
+                    #ontology_def = line.replace('def: ', '')
+                    #print(f'Something is wrong with Ontology ID. Definition was found ({ontology_def}), but no ID or Name was recorded.')
+            
+            else:
+                #ontology_def = line.replace('def: ', '')
+                #print(f'Something wrong. Found Definition ({ontology_def}), but [Term] flag was not recorded.')
+                continue
+
+output_table_obj.close()
