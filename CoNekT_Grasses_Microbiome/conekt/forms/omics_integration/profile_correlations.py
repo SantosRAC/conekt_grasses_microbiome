9b236ee42bf88ae82cf5bd4dfe39204281ef6c97
Renato Santos <renatoacsantos@gmail.com>
2024-09-13 11:08:06 -0400

    Drafting page to show correlations among groups in a particular study

--- CoNekT_Grasses_Microbiome/conekt/forms/omics_integration/profile_correlations.py
+++ CoNekT_Grasses_Microbiome/conekt/forms/omics_integration/profile_correlations.py
@@ -4,8 +4,17 @@ from wtforms.validators import InputRequired
 
 from conekt.models.species import Species
 
+class SearchCorrelatedProfilesStudyGroupsForm(FlaskForm):
+    species_id = SelectField('Species', coerce=int)
+    study_id = SelectField('Study', coerce=int)
+    method_id = SelectField('Method', coerce=int)
+    correlation_cutoff_study_groups = TextAreaField('Correlation Coefficient Cutoff')
 
-class SearchCorrelatedProfilesGroupsForm(FlaskForm):
+    def populate_form(self):
+        self.species_id.choices = [(0, 'Select Species first')] + [(s.id, s.name) for s in Species.query.order_by(Species.name)]
+
+
+class SearchCorrelatedProfilesGroupForm(FlaskForm):
     species_id = SelectField('Species', coerce=int)
     study_id = SelectField('Study', coerce=int)
     method_id = SelectField('Method', coerce=int)
