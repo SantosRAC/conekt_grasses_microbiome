2b6ac463bce508bf887fbaf6b945b57047b239e2
Renato Santos <renatoacsantos@gmail.com>
2024-11-11 10:19:16 -0500

    Removed functions used to compare cross-correlations internally

--- CoNekT_Grasses_Microbiome/conekt/controllers/admin/controls/omics_integration/profile_correlations.py
+++ CoNekT_Grasses_Microbiome/conekt/controllers/admin/controls/omics_integration/profile_correlations.py
@@ -6,47 +6,10 @@ from conekt.extensions import admin_required
 
 from conekt.controllers.admin.controls import admin_controls
 
-from conekt.forms.admin.build_expression_microbiome_correlations import BuildExpMicrobiomeCorrelationsForm
 from conekt.forms.admin.add_expression_microbiome_correlations import AddExpMicrobiomeCorrelationsForm
 from conekt.forms.admin.add_expression_microbiome_correlations_go_enrichment import AddExpMicrobiomeCorrelationsGOEnrichForm
 from conekt.models.expression_microbiome.expression_microbiome_correlation import ExpMicroCorrelation
 from conekt.models.expression_microbiome.pairs_go_enrichment import GroupCorPairsGOEnrichment
-
-@admin_controls.route('/build/profile_correlations', methods=['POST'])
-@admin_required
-def build_profile_correlations():
-    """
-    Controller that will start building correlations for pairs of profiles
-
-    :return: return to admin index
-    """
-
-    form = BuildExpMicrobiomeCorrelationsForm(request.form)
-
-    if request.method == 'POST' and form.validate():
-        study_id = int(request.form.get('study_id'))
-        description = request.form.get('description')
-        tool = request.form.get('tool')
-        stat_method = request.form.get('stat_method')
-        multiple_test_cor_method = request.form.get('multiple_test_cor_method')
-        rnaseq_norm = request.form.get('rnaseq_norm')
-        metatax_norm = request.form.get('metatax_norm')
-        correlation_cutoff = float(request.form.get('correlation_cutoff'))
-        corrected_pvalue_cutoff = float(request.form.get('corrected_pvalue_cutoff'))
-
-        ExpMicroCorrelation.calculate_expression_metataxonomic_correlations(study_id, description, tool, stat_method,
-                                                                    multiple_test_cor_method, rnaseq_norm,
-                                                                    metatax_norm, correlation_cutoff,
-                                                                    corrected_pvalue_cutoff)
-
-        flash('Successfully built correlations between microbiome and transcriptome.', 'success')
-        return redirect(url_for('admin.index'))
-    else:
-        if not form.validate():
-            flash('Unable to validate data, potentially missing fields', 'danger')
-            return redirect(url_for('admin.index'))
-        else:
-            abort(405)
 
 
 @admin_controls.route('/add/profile_correlations', methods=['POST'])
--- CoNekT_Grasses_Microbiome/conekt/controllers/admin/views/omics_integration/expression_microbiome_correlations.py
+++ CoNekT_Grasses_Microbiome/conekt/controllers/admin/views/omics_integration/expression_microbiome_correlations.py
@@ -3,24 +3,8 @@ from flask_admin import expose
 from markupsafe import Markup
 
 from conekt.controllers.admin.views import AdminBaseView
-from conekt.forms.admin.build_expression_microbiome_correlations import BuildExpMicrobiomeCorrelationsForm
 from conekt.forms.admin.add_expression_microbiome_correlations import AddExpMicrobiomeCorrelationsForm
 from conekt.forms.admin.add_expression_microbiome_correlations_go_enrichment import AddExpMicrobiomeCorrelationsGOEnrichForm
-
-
-class BuildCorrelationsView(AdminBaseView):
-    """
-    Admin page to compute correlations (Metataxonomics vs. Expression) of a study to the database
-    """
-
-    @expose('/')
-    def index(self):
-        form = BuildExpMicrobiomeCorrelationsForm()
-        form.populate_species()
-
-        flash('Please use the script in the current version of CoNekT Grasses Microbiome.', 'danger')
-        flash('For Devs: Before enabling this page to work, modify internal functions to match what the script is doing.', 'danger')
-        return self.render('admin/build/profile_correlations.html', form=form)
 
 class AddCorrelationsView(AdminBaseView):
 
