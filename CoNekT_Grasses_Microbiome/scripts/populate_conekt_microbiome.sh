3f0df84eeeedcc92bf780733c94e9d5e2c51af74
Renato Santos <renatoacsantos@gmail.com>
2025-02-18 13:28:09 -0300

    Updating the populate script with path to data

--- CoNekT_Grasses_Microbiome/scripts/populate_conekt_microbiome.sh
+++ CoNekT_Grasses_Microbiome/scripts/populate_conekt_microbiome.sh
@@ -3,7 +3,7 @@
 BASE_DIR=/home/santosrac/Repositories/conekt_grasses_microbiome
 SCRIPTS_DIR=$BASE_DIR/CoNekT_Grasses_Microbiome/scripts
 SPECIES_ARRAY=( Zma )
-#DATA_DIR=
+DATA_DIR=/home/renato/projects/uga_cena/conekt_grasses_microbiome
 #SPECIES_TABLE=$DATA_DIR/Species/species_info.tsv
 
 #Database credentials for CoNekT Grasses from file
@@ -15,55 +15,55 @@ echo "Populating CoNekT Microbiome with taxonomy data"
 $SCRIPTS_DIR/add/add_taxonomy.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
-    --silva_taxonomy_file /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/Taxonomy/silva/138/tax_slv_lsu_138.1.txt\
+    --silva_taxonomy_file $DATA_DIR/taxonomy/silva/tax_slv_lsu_138.1.txt\
     --silva_release 138.1\
-    --gg_taxonomy_file /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/Taxonomy/gg_13_5/gg_13_5_taxonomy.txt\
+    --gg_taxonomy_file $DATA_DIR/taxonomy/gg_13_5/gg_13_5_taxonomy.txt\
     --gg_release 13.5\
-    --gtdb_taxonomy_file /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/Taxonomy/gtdb/214/bac120_taxonomy_r214.tsv\
+    --gtdb_taxonomy_file $DATA_DIR/taxonomy/gtdb/bac120_taxonomy_r214.tsv\
     --gtdb_release 214
 
 echo "Populating CoNekT Microbiome with functional data"
 $SCRIPTS_DIR/add/add_functional_data.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
-    --interpro_xml /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/FunctionalData/interpro.xml\
-    --gene_ontology_obo /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/FunctionalData/go.obo\
-    --cazyme /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/FunctionalData/CAZyDB.07302020.fam-activities.txt
+    --interpro_xml $DATA_DIR/functional_data/interpro.xml\
+    --gene_ontology_obo $DATA_DIR/functional_data/go.obo\
+    --cazyme $DATA_DIR/functional_data/CAZyDB.07302020.fam-activities.txt
 
 echo "Populating CoNekT Microbiome with ontology data"
 $SCRIPTS_DIR/add/add_ontologies.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
-    --plant_ontology /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/Ontology/plant-ontology.txt\
-    --plant_e_c_ontology /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/Ontology/peco.tsv\
-    --envo /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/Ontology/envo_09012024.txt
+    --plant_ontology $DATA_DIR/ontology/plant-ontology.txt\
+    --plant_e_c_ontology $DATA_DIR/ontology/peco.tsv\
+    --envo $DATA_DIR/ontology/envo_09012024.txt
 
 echo "Populating CoNekT Microbiome with species data"
 $SCRIPTS_DIR/add/add_species.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
-    --input_table /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/species_info.tsv
+    --input_table $DATA_DIR/info_species.tsv
 
 echo "Populating CoNekT Microbiome with gene descriptions"
 $SCRIPTS_DIR/add/add_gene_descriptions.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
     --species_code Zma\
-    --gene_descriptions /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/datasets/Maize/Zma_cds_descriptions.txt
+    --gene_descriptions $DATA_DIR/datasets/maize/Zma_cds_descriptions.txt
 
 echo "Populating CoNekT Microbiome with GO annotation"
 $SCRIPTS_DIR/add/add_go.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
     --species_code Zma\
-    --go_tsv /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/datasets/Maize/Zma_gos.tsv\
+    --go_tsv $DATA_DIR/datasets/maize/Zma_gos.tsv\
     --annotation_source "GOs from InterProScan"
 
 echo "Populating CoNekT Microbiome with InterProScan data"
 $SCRIPTS_DIR/add/add_interproscan.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
-    --interproscan_tsv /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/datasets/Maize/Zma_interproscan.tsv\
+    --interproscan_tsv $DATA_DIR/datasets/maize/Zma_interproscan.tsv\
     --species_code Zma
 
 echo "Populating CoNekT Microbiome with samples"
@@ -71,7 +71,7 @@ $SCRIPTS_DIR/add/add_samples.py --db_admin $DB_ADMIN\
     --db_name $DB_NAME\
     --db_password $DB_PASSWORD\
     --species_code Zma\
-    --sample_annotation /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/datasets/Maize/Zma_sample_annotation.txt
+    --sample_annotation $DATA_DIR/datasets/maize/Zma_sample_annotation.txt
 
 echo "Creating a Study for a species and Populating CoNekT Microbiome"
 $SCRIPTS_DIR/build/create_study.py --db_admin $DB_ADMIN\
@@ -81,7 +81,7 @@ $SCRIPTS_DIR/build/create_study.py --db_admin $DB_ADMIN\
     --study_name "Integration of Maize Microbiome and Transcriptome in leaves"\
     --study_description "Integration of Maize Microbiome and Transcriptome in leaves to understand the role of microbiome in plant transcriptome"\
     --study_type expression_metataxonomics\
-    --krona_file /home/santosrac/Repositories/conekt_grasses_microbiome/CoNekT_Grasses_Microbiome/tests/data/microbiome_data/otus/text.krona.html
+    --krona_file $DATA_DIR/datasets/maize/Zma_krona.html
 
  # Populate CoNekT Microbiome with OTUs
  $SCRIPTS_DIR/add/add_otus.py --db_admin $DB_ADMIN\
@@ -96,7 +96,7 @@ $SCRIPTS_DIR/build/create_study.py --db_admin $DB_ADMIN\
     --clustering_threshold 0.97\
     --clustering_reference_db greengenes\
     --clustering_reference_db_release 13_5\
-     --fasta_file /home/santosrac/Projects/UGA_RACS/CoNekT_Grasses_Microbiome/datasets/Maize/rep_set_conekt.fna
+     --fasta_file $DATA_DIR/datasets/maize/rep_set_conekt.fna
 
  # Populate CoNekT Microbiome with OTU classification
 
